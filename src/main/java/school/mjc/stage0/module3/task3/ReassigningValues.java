bc01b0f4e169408c03bb5e24a99d0d95b408498e
polina <polinatuzankina@gmail.com>
2023-07-09 16:24:11 +0600

    module 3 task 3

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,6 +2,35 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first;
+        int second;
+        int third;
+
+        first = 1;
+        second = 10;
+        third = 100;
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+        int linkToFirst;
+        int linkToSecond;
+        int linkToThird;
+
+        linkToFirst = first;
+        linkToSecond = second;
+        linkToThird = third;
+
+        first = 15;
+        second = 6;
+        third = 4;
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
     }
 }
